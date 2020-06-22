48557f71be74f43b71138f9473def261be673c76
Krzysztof Fila <kfila17@gmail.com>
2020-06-22 19:59:31 +0200

    Fixing initActions in class AmountWidget

--- src/js/script.js
+++ src/js/script.js
@@ -208,9 +208,11 @@
     constructor(element) {
       const thisWidget = this;
       thisWidget.getElements(element);
-      thisWidget.setVaule(thisWidget.input.value);
+      thisWidget.setValue(thisWidget.input.value);
+      thisWidget.initActions();
+
       console.log('AmountWidget:', thisWidget);
-      console.log('constructor elements:', element);
+      console.log('constructor arguments:', element);
     }
     getElements(element) {
       const thisWidget = this;
@@ -219,20 +221,38 @@
       thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
       thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
     }
-    setVaule() {
+    setValue(value) {
       const thisWidget = this;
       const newValue = parseInt(value);
       /* TODO: Add validation */
       thisWidget.value = newValue;
-      thisWidget.input.value = thisWidget.value;
+      thisWidget.announce();
+      thisWidget.input.value = thisWidget.value; // CHECK THIS
+    }
+    initActions() {
+      const thisWidget = this;
+      thisWidget.input.addEventListener('change', function () {
+        thisWidget.setValue(thisWidget.input.value);
+      });
+      thisWidget.linkDecrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value - 1);
+      });
+      thisWidget.linkIncrease.addEventListener('click', function (event) {
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value + 1);
+      });
+    }
+    announce() {
+      const thisWidget = this;
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
   const app = {
     initMenu: function () {
       const thisApp = this;
-
       // console.log('thisApp.data:', thisApp.data);
-
       /*Tworzenie instancji dla każdego produktu(w pętli) po obiekcie thisApp.data.products*/
       for (let productData in thisApp.data.products) {
         new Product(productData, thisApp.data.products[productData]);
@@ -252,7 +272,7 @@
       // console.log('*** App starting ***');
       // console.log('thisApp:', thisApp);
       // console.log('classNames:', classNames);
-      // console.log('settings:', settings);
+      console.log('settings:', settings);
       // console.log('templates:', templates);
       /*dodawanie delkaracji*/
       thisApp.initData();
@@ -260,8 +280,6 @@
       thisApp.initMenu();
     },
   };
-
-
   /*DEKLARACJA APP*/
   app.init();
   app.initData();
